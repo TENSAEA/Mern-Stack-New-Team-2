ce57aeaaebea17b4e52a78a8dad29b1025a87681
aman7ph <aman7ph@gmail.com>
2024-03-29 19:31:46 +0300

    applaying some change

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/v1//reports", reportRoutes);
 const connectDb = require("./config/db");
 connectDb();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
